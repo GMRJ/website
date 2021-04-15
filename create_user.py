213d5526822a489602bb069aec1b406b98cd37d4
GMRJ <74367297+GMRJ@users.noreply.github.com>
2021-04-15 10:05:35 -0400

    Add Create User
    I forgot to add it

---
+++ create_user.py
@@ -0,0 +1,28 @@
+import sqlite3
+import secrets
+import hashlib
+
+def main():
+    # Step 1: Prompt user for info
+    conn = sqlite3.connect("tweet_db.db")
+    username = input("Enter Username: ")
+    password = input("Enter Password: ")
+    print("User: %s, with password %s" % (username, password))
+    # Step 2: Generate salt
+    salt = secrets.token_hex(6)
+    # Step 3: Add salt to password
+    salted_password = password+salt
+    # Step 4: HASH IT
+    hash = hashlib.md5(salted_password.encode('ascii')).hexdigest()
+    conn.execute("INSERT INTO users (username, password, salt) VALUES (?,?,?)", (username, hash, salt))
+
+    print("salt: %s" % salt)
+    cursor = conn.cursor()
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+
+
+main()
