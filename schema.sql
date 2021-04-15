f0fae4bfc27705db045fc3c9ceb8836c70f10ad8
GMRJ <74367297+GMRJ@users.noreply.github.com>
2021-04-14 23:12:35 -0400

    Added login, logout, and delete
    Added login, logout, and delete

--- schema.sql
+++ schema.sql
@@ -1,8 +1,15 @@
-CREATE TABLE "tweet_db" (
+CREATE TABLE IF NOT EXISTS "tweet_db" (
 	"ID"	INTEGER NOT NULL UNIQUE,
 	"content"	TEXT NOT NULL,
 	"time"	INTEGER,
 	"location"	TEXT,
 	"likes"	INTEGER,
 	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "users" (
+    "username" TEXT NOT NULL UNIQUE,
+    "password" TEXT,
+    "cookie" TEXT,
+    "salt" TEXT
 );
